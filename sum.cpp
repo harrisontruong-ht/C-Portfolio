30f411a7f47690ac2ac6fcbf4d6c4afe04f72e43
harrisontruong-ht <harrison.truong@gmail.com>
2025-09-01 23:52:19 -0700

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+
+int userInt1;
+int userInt2;
+int userInt3;
+
+        cout << "Enter integer one: " << endl;
+        cin >> userInt1;
+        cout << "Enter integer two: " << endl;
+        cin >> userInt2;
+        cout << "Enter integer three: " << endl;
+        cin >> userInt3;
+        cout << userInt1 << " + " << userInt2 << " + " << userInt3 << " = " << >
+        return 0;
+}
