a146b63bde6d7ea91b35dddd703606ad247c2af6
harrisontruong-ht <harrison.truong@gmail.com>
2025-11-30 00:32:36 -0800

    Implement CPizza demonstration program
    This program demonstrates the CPizza class by creating multiple CPizza objects, prompting the user for an order, calculating costs, and displaying order details.

---
+++ Introduction/EC_Pizza/main.cpp
@@ -0,0 +1,43 @@
+// =============================================================
+// file: main.cpp
+// =============================================================
+// Programmer: Harrison Truong
+// Date: 11/23/25
+// Class: CSCI 123 ("Introduction to Programming using C++")
+// Time: Online
+// Instructor: Dr. Rodriguez
+// Project: CPizza Demonstration
+// Description:
+//      This program demonstrates the CPizza class by creating
+//      multiple CPizza objects using different constructors,
+//      prompting the user for an order, calculating costs,
+//      and displaying the order details.
+// =============================================================
+#include <iostream>
+#include "cpizza.h"
+using namespace std;
+
+int main()
+{
+        CPizza  order1; // default CTOR
+
+        order1.promptOrder();
+
+        CPizza order2(2,1,3); // type CTOR
+        CPizza order3(order2); //copy CTOR
+
+        order1.calcCost();
+        order2.calcCost();
+        order3.calcCost();
+
+        cout << "\nOrder 1:\n\n";
+        order1.dispOrder();
+
+        cout << "\nOrder 2:\n\n";
+        order2.dispOrder();
+
+        cout << "\nOrder 3:\n\n";
+        order3.dispOrder();
+
+        return 0;
+}
