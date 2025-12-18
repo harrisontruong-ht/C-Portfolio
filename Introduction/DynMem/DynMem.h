e5e59f5c4a19b0117b678e343d1285a205111384
harrisontruong-ht <harrison.truong@gmail.com>
2025-12-18 13:19:09 -0800

    Add function prototypes for dynamic memory operations

---
+++ Introduction/DynMem/DynMem.h
@@ -0,0 +1,5 @@
+//function prototypes
+int* AllocArray(int numElements);
+void InitArray(int *ptr, int numElements);
+void DispArray(int *ptr, int numElements);
+void SortArray(int *ptr, int numElements);
